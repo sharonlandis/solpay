224d2046d7060c44bb2bbcff3c50f8553d88865d
sharonlandis <sharonrlandis@gmail.com>
2022-06-21 19:34:55 -0400

    initial

---
+++ pages/index.js
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from "react";
+import CreateProduct from "../components/CreateProduct";
+import Product from "../components/Product";
+import HeadComponent from "../components/Head";
+// import { PublicKey } from "@solana/web3.js";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
+// Constants
+const TWITTER_HANDLE = "sharonrlandis";
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+  // This will fetch the users' public key (wallet address) from any wallet we support
+  const { publicKey } = useWallet();
+  const isOwner = publicKey
+    ? publicKey.toString() === process.env.NEXT_PUBLIC_OWNER_PUBLIC_KEY
+    : false;
+  const [creating, setCreating] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img
+        className="connect-img"
+        // src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif"
+        src="https://media2.giphy.com/media/2yLNN4wTy7Zr8JSXHB/200w.webp?cid=ecf05e47xhermh7oc23eand2infytegxkhfupn7os2m0fql3&rid=200w.webp&ct=g"
+        alt="doggos"
+      />
+      <div className="button-container">
+        <WalletMultiButton className="cta-button connect-wallet-button" />
+      </div>
+    </div>
+  );
+
+  useEffect(() => {
+    if (publicKey) {
+      fetch(`/api/fetchProducts`)
+        .then((response) => response.json())
+        .then((data) => {
+          setProducts(data);
+          console.log("Products", data);
+        });
+    }
+  }, [publicKey]);
+
+  const renderItemBuyContainer = () => (
+    <div className="products-container">
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  );
+
+  return (
+    <div className="App">
+      <HeadComponent />
+      <div className="container">
+        <header className="header-container">
+          <p className="header"> Doggos Dog Store </p>
+          <p className="sub-text">
+            Home of the best ideas on planet earth on how to spoil your dog{" "}
+          </p>
+
+          {/* Make form public  */}
+          {/* {isOwner && (
+            <button
+              className="create-product-button"
+              onClick={() => setCreating(!creating)}
+            >
+              {creating ? "Close" : "Create Product"}
+            </button>
+          )} */}
+
+          {publicKey && (
+            <button
+              className="create-product-button"
+              onClick={() => setCreating(!creating)}
+            >
+              {creating ? "Close" : "Create Product"}
+            </button>
+          )}
+        </header>
+
+        <main>
+          {creating && <CreateProduct />}
+          {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
+        </main>
+
+        <div className="footer-container">
+          <img
+            alt="Twitter Logo"
+            className="twitter-logo"
+            src="twitter-logo.svg"
+          />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built on @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
